d38efa4d0c0773261ff5d0d1f264ab07b1fade20
AWonglice <3042824300@qq.com>
2017-07-08 11:38:10 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function has two variables in total: x and m
+## x is the matrix that needed to be modified
+## m is used for storing the result
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
 
+  get<-function()x
+  list(set=set,
+       get=get,
+       setSolve=setSolve,
+       getSolve=getSolve)
 }
 
 
-## Write a short comment describing this function
+## input variable x must be a makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
